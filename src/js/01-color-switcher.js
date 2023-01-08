b6f6ab64024b47e1d8798f63f6747e63a9fe8632
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-01-08 18:49:38 +0200

    make 9HW part 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+let btnStartEl = document.querySelector('[data-start]');
+let btnStopEl = document.querySelector('[data-stop]');
+let bodyEl = document.querySelector("body");
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+btnStartEl.addEventListener('click', onStartClick);
+
+function onStartClick(){
+    // console.log("start click");
+    
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    btnStartEl.disabled = true;
+    timerId = setInterval(() => {
+        getRandomHexColor();
+        bodyEl.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+}
+
+btnStopEl.addEventListener('click', onStopClick);
+
+function onStopClick(){
+    // console.log("STOP click");
+    btnStartEl.disabled = false;
+    clearInterval(timerId);
+}
