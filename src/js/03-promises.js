4b5d0fbe9270d6bb99be29f27a7bc057c23d2932
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-01-09 20:20:18 +0200

    make 9 HW task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,76 @@
+
+let formEl = document.querySelector('.form')
+let delayEl = document.querySelector('.form [name="delay"]');
+let stepEl = document.querySelector('.form [name="step"]');
+let amountEl = document.querySelector('.form [name="amount"]');
+
+const formToKey = "feedback-form-state";
+
+formEl.addEventListener('input', onInput);
+formEl.addEventListener('submit', onSubmit);
+
+let inLocal = localStorage.getItem(formToKey);
+let parsed = JSON.parse(inLocal);
+
+function onInput(){
+  console.log("input");
+    inLocal = localStorage.getItem(formToKey);
+    parsed = JSON.parse(inLocal);
+    console.log(parsed);
+    localStorage.setItem(formToKey, JSON.stringify({
+               delay : delayEl.value,
+               step : stepEl.value,
+               amount : amountEl.value,
+           }));
+}
+
+function onSubmit(e){
+  e.preventDefault();
+  // console.log("submit");
+  inLocal = localStorage.getItem(formToKey);
+    parsed = JSON.parse(inLocal);
+    // console.log(`{delay:${delayEl.value}, step:${stepEl.value}, amount: ${amountEl.value}}`);
+    let delay = Number(delayEl.value)
+    let step = Number(stepEl.value)
+    let amount = Number(amountEl.value);
+    
+    for (let i = 1; i <= amount; i += 1){
+          const delayStep = delay + step * (i - 1);
+          createPromise(i, delayStep).then(onSuccess).catch(onError)
+        };
+      };
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        fulfill({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+};
+
+function onSuccess({ position, delay }) { 
+ console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+function onError({ position, delay }) {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+};
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+
+
+
