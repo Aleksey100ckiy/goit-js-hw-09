b6a6873fd690b97735cfeb73484eda9c61c4e7ea
Aleksey100ckiy <109811250+Aleksey100ckiy@users.noreply.github.com>
2023-01-10 19:38:15 +0200

    намагається зробити 2 задача

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,24 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from "flatpickr";
 
+import "flatpickr/dist/flatpickr.min.css";
+
+const flatpickr = require("flatpickr");
+
+flatpickr("#datetime-picker");
+
+// let timeField = document.querySelector('.timer');
+
+// timeField.addEventListener('input', onTimeInput);
+// console.log(onTimeInput);
+
+// console.log(timeField);
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
